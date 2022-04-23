c3bf1196301d26fd344c46e84f270527fb45da2f
haroldeduardo <hehurtado9@misena.edu.co>
2022-04-23 10:02:45 -0500

    primer commit

---
+++ database/migrations/2022_04_23_141555_create_funcionario_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->id();
+            $table->string ('nombre',50)->unique();
+            $table->string ('descripcion',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionario');
+    }
+}
